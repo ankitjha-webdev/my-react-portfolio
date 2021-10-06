8a0e3349a47c3001e8628879b1d5e729ef66ab2a
ankitjha-webdev <190101120091@cutm.ac.in>
2021-10-07 01:29:30 +0530

    portfolio website

---
+++ src/data.js
@@ -0,0 +1,69 @@
+export const projects = [
+  {
+    title: "Covid19_Teacker_Application",
+    subtitle: "Android and Java",
+    description:
+      "The real time covid19 data fetching application which has many features like the application can show the real time conform Caes, Active Cases, Recover Cases and many more..",
+    image: "https://ca-times.brightspotcdn.com/dims4/default/bfccd8b/2147483647/strip/true/crop/3000x1655+0+0/resize/1486x820!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F85%2Fff%2Fdd01791e96731ce838b07c8e511c%2Fda599c37ec1b4f15824bb9806e6a66c2",
+    link: "https://github.com/ankitjha-webdev/Covid19_Teacker_Application",
+  },
+  {
+    title: "Credit card Fraud Detection",
+    subtitle: "Python and Data Science library",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://dataaspirant.com/wp-content/uploads/2020/09/1-Credit-card-fraud-detection-with-classification-algorithms.png",
+    link: "https://github.com/ankitjha-webdev/Credit_Card_Fraud_Detection",
+  },
+  {
+    title: "World-population-data-analyzation-and-visualization ",
+    subtitle: "Python and Data Science library",
+    description:
+      "this is my personal portfolio which is develop in react with external javascript and css it has some extra features which makes it diffrents from other portfolio websites like visitor counteer, Google Captcha and many more...",
+    image: "./PORTFOLIO.png",
+    link: "https://github.com/ankitjha-webdev/World-population-data-analyzation-and-visualization",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      " “You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change and click save.Thanks, guys!” ",
+    image: "./profile-img.png",
+    name: "Ankit Kumar",
+    company: "cutm",
+  },
+  {
+    quote:
+      "“Wow. I just updated my site and it was SO SIMPLE. I am blown away. You guys truly kick ass. Thanks for being so awesome. High fives!”",
+    image: "./about-img.png",
+    name: "Ruben Alvarez",
+    company: "google",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Android",
+  "Python",
+  "Java",
+  "C",
+  "C++",
+  "Wordpress",
+  "SEO",
+  "Php",
+  "Bootstrap",
+  "Html",
+  "CSS",
+  "Aws",
+
+];
